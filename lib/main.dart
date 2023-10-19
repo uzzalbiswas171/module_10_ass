a6d32414e004556effde1ee765fc24fcec0c76c9
Your Name <you@example.com>
2023-10-19 21:33:20 +0600

    hadi

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SelectionScreen(),
+      home: ItemListScreen(),
     );
   }
 }
 
-class SelectionScreen extends StatefulWidget {
+class ItemListScreen extends StatefulWidget {
   @override
-  _SelectionScreenState createState() => _SelectionScreenState();
+  _ItemListScreenState createState() => _ItemListScreenState();
 }
 
-class _SelectionScreenState extends State<SelectionScreen> {
-  List<String> items1=["sdf"] ;
-  List<String> items2 =["sad"];
-  TextEditingController _TE1=TextEditingController();
-  TextEditingController _TE2=TextEditingController();
-  Set<String> selectedItems = Set<String>();
+class _ItemListScreenState extends State<ItemListScreen> {
+  List<Item> items = [
+    Item("Item 1"),
+    Item("Item 2"),
+    Item("Item 3"),
+    Item("Item 4"),
+    Item("Item 5"),
+  ];
+
+  int selectedCount = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -32,70 +35,58 @@ class _SelectionScreenState extends State<SelectionScreen> {
       appBar: AppBar(
         title: Text('Selection Screen'),
       ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: EdgeInsets.all(
-          20
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 10,),
-           TextFormField(
-             controller: _TE1,
-             decoration: InputDecoration(
-               hintText: "Type here"
-             ),
-           ),
-            SizedBox(height: 10,),
-            TextFormField(
-              controller: _TE2,
-             decoration: InputDecoration(
-               hintText: "Type here"
-             ),
-           ), SizedBox(height: 10,),
-
-            Container(
-              height: 50,
-              width: double.infinity,
-            //  color: Colors.green,
-              alignment: Alignment.topRight,
-              child: ElevatedButton(onPressed: () {
-                setState(() {
-                  items1.add(_TE1.text);
-                  items2.add(_TE2.text);
-                });
-              }, child: Text("Add")),
-            ),
-            SizedBox(height: 10,),
-            Expanded(flex: 3,child: Container(
-              child: ListView.builder(
-                  itemCount: items1.length,
-                itemBuilder:(context, index) {
-                return Card(
-                  color: Colors.amber.shade50,
-                  child: ListTile(
-                    leading:Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                          color: Colors.lightGreenAccent,
-                          borderRadius: BorderRadius.circular(30)),
-                    ) ,
-
-                    title: Text("${items1[index]}"),
-                    subtitle: Text("${items1[index]}"),
-                    trailing: IconButton(onPressed: () {
-                      
-                    }, icon: Icon(Icons.arrow_circle_right_outlined)),
-                  ),
-                );
-              },),
-              )),
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index].name),
+            tileColor: items[index].selected ? Colors.blue : null,
+            onTap: () {
+              setState(() {
+                items[index].selected = !items[index].selected;
+                if (items[index].selected) {
+                  selectedCount++;
+                } else {
+                  selectedCount--;
+                }
+              });
+            },
+          );
+        },
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showSelectionDialog();
+        },
+        child: Icon(Icons.check),
+      ),
     );
   }
+
+  void _showSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Selected Items'),
+          content: Text('Number of selected items: $selectedCount'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Item {
+  final String name;
+  bool selected;
+
+  Item(this.name, {this.selected = false});
 }
